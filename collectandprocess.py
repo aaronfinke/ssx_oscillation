ca0d5795df4b601740e45116d3e1e9349948df41
Aaron Finke <af554@mc224.lns.cornell.edu>
2019-01-22 15:13:20 -0500

    changed setup of master directories

--- collectandprocess.py
+++ collectandprocess.py
@@ -1,49 +1,51 @@
 import os, sys, h5py
 from argparse import ArgumentParser
 from make_xds_template import *
-from setup import is_valid_h5_file
+import setup
 
 
 def main():
-  """
-  Main routine for serialCAP
-  """
-  parser = ArgumentParser(description="write to a file")
+    """
+    Main routine for serialCAP
+    """
+    parser = ArgumentParser(description="write to a file")
 
-  parser.add_argument("-i","--input", type=is_valid_h5_file, required=True, nargs='+',
+    parser.add_argument("-i","--input", type=setup.is_valid_h5_file, required=True, nargs='+',
     help="path(s) of HDF5 master file(s)")
 
-  parser.add_argument("-b","--beamcenter", nargs=2, required=True,
+    parser.add_argument("-b","--beamcenter", nargs=2, required=True,
     help="beam center in X and Y (two arguments)")
 
-  parser.add_argument("-o","--oscillation", type=float, default=0.2,
+    parser.add_argument("-r","--oscillation", type=float, default=0.2,
     help="oscillation angle per frame, default = 0.2")
 
-  parser.add_argument("-d","--distance", type=float, default=100,
+    parser.add_argument("-d","--distance", type=float, default=100,
     help="detector distance in mm")
 
-  parser.add_argument("-w","--wavelength", type=float, default=1.216,
+    parser.add_argument("-w","--wavelength", type=float, default=1.216,
     help="Wavelength in Angstrom, default is 1.216")
 
-  parser.add_argument("-f","--frames", type=int, default=5,
-    help="Number of frames per degree, default is 5") 
+    parser.add_argument("-f","--frames", type=int, default=5,
+    help="Number of frames per degree, default is 5")
 
-  parser.add_argument("-t","--totalframes", type=int, default=0,
+    parser.add_argument("-t","--totalframes", type=int, default=0,
     help="Total number of frames to be processed, default all")
 
-  parser.add_argument("--output", type=str, default=os.getcwd(),
+    parser.add_argument("--output", default=os.getcwd(),
     help="Use this option to change output directory, default pwd")
 
-  parser.add_argument("-sg","--spacegroup", type=int, default=0,
+    parser.add_argument("-sg","--spacegroup", type=int, default=0,
     help="Space group")
 
-  parser.add_argument("-u","--unitcell", type=str, default="50 50 50 90 90 90",
+    parser.add_argument("-u","--unitcell", type=str, default="50 50 50 90 90 90",
     help="unit cell")
 
-  argslist = parser.parse_args()
+    argslist = parser.parse_args()
+
+    setup.setup(argslist)
 
 
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@ import sys
 from pathlib import Path
 
 def is_valid_h5_file(path):
-  """ 
-  Checks to ensure that the file path(s) is/are hdf5 files
-  """
-  try:
-    file = h5py.File(path, 'r')
-  except IOError:
-    raise argparse.ArgumentTypeError("File is not valid h5 format")
-  return path
+    """
+    Checks to ensure that the file path(s) is/are hdf5 files
+    """
+    try:
+        file = h5py.File(path, 'r')
+    except IOError:
+        raise argparse.ArgumentTypeError("File is not valid h5 format")
+    return path
 
 def get_h5_file(path):
-  try:
-    file = h5py.File(path, 'r')
-  except Exception:
-    raise IOError("Not a valid h5 file")
-  return file
+    try:
+        file = h5py.File(path, 'r')
+    except Exception:
+        raise IOError("Not a valid h5 file")
+    return file
 
 def getNumberOfFiles(path):
     """Return number of data files linked to a master file. Much
-    slower than listing the number of files in a master file but 
+    slower than listing the number of files in a master file but
     also checks for correct type."""
     f = get_h5_file(path)
     group = f['/entry/data']
@@ -35,24 +35,26 @@ def getNumberOfFiles(path):
     return num
 
 def getNumberOfFiles_fast(path):
-  """return number of data files by simply
-  finding the length of the H5 group. No checks."""
-  f = get_h5_file(file) 
-  return len(f['/entry/data'])
+    """return number of data files by simply
+    finding the length of the H5 group. No checks."""
+    f = get_h5_file(path)
+    return len(f['/entry/data'])
 
 def getNumberOfFilesToProcess(path,num=None):
-  if num == None:
-    return getNumberOfFiles_fast(path)
-  else:
-    return num
+    if num == None:
+        return getNumberOfFiles_fast(path)
+    else:
+        return num
 
-def setupMasterList(arglist):
-  masterlist = []
-  for masterpath in arglist:
-    masterlist.append(Path(masterpath.strip('_master.h5')))
-  return masterlist
+def getMasterPrefix(arg):
+    stripped = Path(arg.strip('_master.h5'))
+    head, tail = os.path.split(stripped)
+    return tail
 
-def setupMasterDirectories(arglist):
-  for masterpath in arglist:
-    masterpath.mkdir(exist_ok=True)
+def setupMasterDirectory(input, dir):
+    master = getMasterPrefix(input)
+    os.mkdir(os.path.join(dir,master))
 
+def setup(argslist):
+    for master in argslist.input:
+        setupMasterDirectory(master,argslist.output)
